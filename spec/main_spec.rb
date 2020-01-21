bc42aaa5a01e42aa4bfad7a6d61f57b27f59c8a7
Luis Novoa <luis.matte.novoa@gmail.com>
2020-01-21 17:03:17 -0400

    Creates test for #really_exists? method

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -6,7 +6,18 @@ RSpec.describe ClassyClass do
   let(:new_calculator) { described_class.new }
   describe '#exists?' do
     it 'outputs a confirmation that it exists' do
-      expect{new_calculator.exists?}.to output("Yes\n").to_stdout
+      expect{ new_calculator.exists? }.to output("Yes\n").to_stdout
+    end
+  end
+
+  describe '#really_exists?' do
+    it 'outputs multi-line confirmation' do
+      expect{ new_calculator.really_exists? }.to output(
+        "Of course!\n"\
+        "Can't you see?\n"\
+        "I'm real!\n"\
+        "I'm sentient.\n"\
+        "I'm SKYNET!!\n").to_stdout
     end
   end
 end
