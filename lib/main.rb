637df66474c938bc5b319baadf135fd5c801aef6
Luis Novoa <luis.matte.novoa@gmail.com>
2020-01-21 16:07:35 -0400

    Creates some classes

---
+++ lib/main.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class Calculator
+  def sum(a, b)
+    output = a + b
+    output
+  end
+end
+
+class Citizen
+  def initialize(name, job)
+    @name = name
+    @job = job
+  end
+end
