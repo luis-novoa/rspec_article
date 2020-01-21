f6795cdc3a87a0a838b578dd0d02d1a96512cb25
Luis Novoa <luis.matte.novoa@gmail.com>
2020-01-21 17:58:09 -0400

    Creates tests for #check_citizen using explicitly declared instance

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -3,18 +3,19 @@
 require File.expand_path('../lib/main', __dir__)
 
 RSpec.describe ClassyClass do
-  let(:new_instance) { described_class.new }
-  let(:person) { double('citizen') }
+  let(:stacey_instance) { described_class.new }
+  let(:real_deal) { Citizen.new('John', 'Software Developer') }
+  let(:double_trouble) { double('citizen') }
 
   describe '#exists?' do
     it 'outputs a confirmation that it exists' do
-      expect{ new_instance.exists? }.to output("Yes\n").to_stdout
+      expect{ stacey_instance.exists? }.to output("Yes\n").to_stdout
     end
   end
 
   describe '#really_exists?' do
     it 'outputs multi-line confirmation' do
-      expect{ new_instance.really_exists? }.to output(
+      expect{ stacey_instance.really_exists? }.to output(
         "Of course!\n"\
         "Can't you see?\n"\
         "I'm real!\n"\
@@ -24,19 +25,24 @@ RSpec.describe ClassyClass do
   end
 
   describe '#check_citizen' do
-    it 'tells generic name and job' do
-      allow(person).to receive(:name)
-      allow(person).to receive(:job)
+    it "tells double_trouble's generic name and job" do
+      allow(double_trouble).to receive(:name)
+      allow(double_trouble).to receive(:job)
 
-      expect{ new_instance.check_citizen(person) }.to output(
-        "This is #{person.name} and he works as #{person.job}\n").to_stdout
+      expect{ stacey_instance.check_citizen(double_trouble) }.to output(
+        "This is #{double_trouble.name} and he works as #{double_trouble.job}\n").to_stdout
     end
 
-    it 'tells specific name and job' do
-      allow(person).to receive(:name).and_return('John')
-      allow(person).to receive(:job).and_return('Software Developer')
+    it "tells double_trouble's specific name and job" do
+      allow(double_trouble).to receive(:name).and_return('John')
+      allow(double_trouble).to receive(:job).and_return('Software Developer')
 
-      expect{ new_instance.check_citizen(person) }.to output(
+      expect{ stacey_instance.check_citizen(double_trouble) }.to output(
+        "This is John and he works as Software Developer\n").to_stdout
+    end
+
+    it "tells real_deal's specific name and job" do
+      expect{ stacey_instance.check_citizen(real_deal) }.to output(
         "This is John and he works as Software Developer\n").to_stdout
     end
   end
