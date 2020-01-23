6950de2526aa89ba5b203b7d9eb6c649f7248869
Luis Novoa <luis.matte.novoa@gmail.com>
2020-01-23 18:19:52 -0400

    Fixes rubocop errors on main_spec.rb

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -10,18 +10,19 @@ RSpec.describe ClassyClass do
 
   describe '#exists?' do
     it 'outputs a confirmation that it exists' do
-      expect{ stacey_instance.exists? }.to output("Yes\n").to_stdout
+      expect { stacey_instance.exists? }.to output("Yes\n").to_stdout
     end
   end
 
   describe '#really_exists?' do
     it 'outputs multi-line confirmation' do
-      expect{ stacey_instance.really_exists? }.to output(
+      expect { stacey_instance.really_exists? }.to output(
         "Of course!\n"\
         "Can't you see?\n"\
         "I'm real!\n"\
         "I'm sentient.\n"\
-        "I'm SKYNET!!\n").to_stdout
+        "I'm SKYNET!!\n"
+      ).to_stdout
     end
   end
 
@@ -30,42 +31,44 @@ RSpec.describe ClassyClass do
       allow(double_trouble).to receive(:name)
       allow(double_trouble).to receive(:job)
 
-      expect{ stacey_instance.check_citizen(double_trouble) }.to output(
-        "This is #{double_trouble.name} and he works as #{double_trouble.job}\n").to_stdout
+      expect { stacey_instance.check_citizen(double_trouble) }.to output(
+        "This is #{double_trouble.name} and he works as #{double_trouble.job}\n"
+      ).to_stdout
     end
 
     it "tells double_trouble's specific name and job" do
       allow(double_trouble).to receive(:name).and_return('John')
       allow(double_trouble).to receive(:job).and_return('Software Developer')
 
-      expect{ stacey_instance.check_citizen(double_trouble) }.to output(
-        "This is John and he works as Software Developer\n").to_stdout
+      expect { stacey_instance.check_citizen(double_trouble) }.to output(
+        "This is John and he works as Software Developer\n"
+      ).to_stdout
     end
 
     it "tells double_trouble's specific name and job using expect" do
       expect(double_trouble).to receive(:name).and_return('John')
       expect(double_trouble).to receive(:job).and_return('Software Developer')
 
-      expect{ stacey_instance.check_citizen(double_trouble) }.to output(
-        "This is John and he works as Software Developer\n").to_stdout
+      expect { stacey_instance.check_citizen(double_trouble) }.to output(
+        "This is John and he works as Software Developer\n"
+      ).to_stdout
     end
 
     it "tells real_deal's specific name and job" do
-      expect{ stacey_instance.check_citizen(real_deal) }.to output(
-        "This is John and he works as Software Developer\n").to_stdout
+      expect { stacey_instance.check_citizen(real_deal) }.to output(
+        "This is John and he works as Software Developer\n"
+      ).to_stdout
     end
   end
 
-  # describe '#sum' do
-    
-  # end
-
   describe '#change_name' do
-    let(:input) {StringIO.new('Larry')}
+    let(:input) { StringIO.new('Larry') }
     let(:new_deal) { Citizen.new('Mark', 'Banker') }
     it "receive user input and change citizen's name" do
       $stdin = input
-      expect{ stacey_instance.change_name(new_deal) }.to output("What's the new name of the citizen?\n").to_stdout.and change { new_deal.name }.to('Larry')
+      expect { stacey_instance.change_name(new_deal) }.to output(
+        "What's the new name of the citizen?\n"
+      ).to_stdout.and change { new_deal.name }.to('Larry')
 
       $stdin = STDIN
     end
